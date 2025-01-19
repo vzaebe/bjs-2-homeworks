5163ce9fe3713c1ebf4f6d1db18a7e4da62e3c96
John Doe <node@gmail.com>
2025-01-19 18:45:52 +0300

    выполнено дз 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+  }
   
-}
+  Student.prototype.setSubject = function(subjectName) {
+    if (!this.excluded) { 
+      this.subject = subjectName;
+    }
+  };
+  
+  Student.prototype.addMarks = function(...marksToAdd) {
+    if (!this.excluded && Array.isArray(this.marks)) {
+      for (let mark of marksToAdd) {
+        this.marks.push(mark);
+      }
+    }
+  };
+  
+  Student.prototype.getAverage = function() {
+    if (!this.marks || !this.marks.length) {
+        return 0;
+    }
+    
+    let sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+    return sum / this.marks.length;
+  };
+  
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+  Student.prototype.exclude = function(reason) {
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
-}
+  };
